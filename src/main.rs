f45931a466b4f1814446344c6c5f3dfceb33a288
Matheus Alves <mathalves@proton.me>
2024-01-18 15:04:16 -0300

    added static files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 mod models;
 
-use axum::{routing::get, Router};
+use axum::Router;
+use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().nest_service("/", ServeDir::new("static"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn root() -> &'static str {
-    "Hello, World!"
 }
