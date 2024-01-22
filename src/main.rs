6a3d0fe40cac37ee17eef673fbbdb312095fd07d
Matheus Alves <mathalves@proton.me>
2024-01-21 21:14:01 -0300

    added ws

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,101 @@
 mod models;
 
-use axum::Router;
+use std::{net::SocketAddr, ops::ControlFlow};
+
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        ConnectInfo, WebSocketUpgrade,
+    },
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use axum_extra::{headers::UserAgent, TypedHeader};
+use futures_util::{SinkExt, StreamExt};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().nest_service("/", ServeDir::new("static"));
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("static"))
+        .route("/ws", get(ws_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
+}
+
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    user_agent: Option<TypedHeader<UserAgent>>,
+    addr: ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    let addr = addr.to_string();
+    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
+        user_agent.to_string()
+    } else {
+        String::from("Unknown browser")
+    };
+    println!("`{user_agent}` at {addr} connected.");
+    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+}
+
+async fn handle_socket(mut socket: WebSocket, who: String) {
+    if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
+        println!("Pinged {who}...");
+    } else {
+        println!("Could not send ping {who}!");
+        return;
+    }
+
+    let (mut _sender, mut receiver) = socket.split();
+
+    let _recv_task = tokio::spawn(async move {
+        let mut cnt = 0;
+        while let Some(Ok(msg)) = receiver.next().await {
+            cnt += 1;
+            // print message and break if instructed to do so
+            if process_message(msg, &who).is_break() {
+                break;
+            }
+        }
+        cnt
+    });
+}
+
+fn process_message(msg: Message, who: &str) -> ControlFlow<(), ()> {
+    match msg {
+        Message::Text(t) => {
+            println!(">>> {who} sent str: {t:?}");
+        }
+        Message::Binary(d) => {
+            println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
+        }
+        Message::Close(c) => {
+            if let Some(cf) = c {
+                println!(
+                    ">>> {} sent close with code {} and reason `{}`",
+                    who, cf.code, cf.reason
+                );
+            } else {
+                println!(">>> {who} somehow sent close message without CloseFrame");
+            }
+            return ControlFlow::Break(());
+        }
+
+        Message::Pong(v) => {
+            println!(">>> {who} sent pong with {v:?}");
+        }
+        Message::Ping(v) => {
+            println!(">>> {who} sent ping with {v:?}");
+        }
+    }
+    ControlFlow::Continue(())
 }
--- src/models/message.rs
+++ src/models/message.rs
@@ -1,30 +1,30 @@
 use chrono::Utc;
 
-pub struct Message {
+pub struct ChatMessage {
     pub room_id: String,
     pub username: String,
     pub text: String,
-    pub kind: MessageKind,
+    pub kind: ChatMessageKind,
     pub sent_at: i64,
 }
 
-pub enum MessageKind {
+pub enum ChatMessageKind {
     Normal,
     Join,
     Left,
 }
 
-impl Message {
-    pub fn new(room_id: &str, username: &str, text: &str, kind: MessageKind) -> Self {
+impl ChatMessage {
+    pub fn new(room_id: &str, username: &str, text: &str, kind: ChatMessageKind) -> Self {
         let mut modified_text = String::from(text);
 
         match kind {
-            MessageKind::Join => modified_text = format!("{} joined the chat.", username),
-            MessageKind::Left => modified_text = format!("{} left the chat.", username),
-            MessageKind::Normal => {}
+            ChatMessageKind::Join => modified_text = format!("{} joined the chat.", username),
+            ChatMessageKind::Left => modified_text = format!("{} left the chat.", username),
+            ChatMessageKind::Normal => {}
         }
 
-        Message {
+        ChatMessage {
             room_id: String::from(room_id),
             username: String::from(username),
             text: modified_text,
